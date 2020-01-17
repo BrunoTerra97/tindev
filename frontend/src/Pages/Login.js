6987c69f5b790e2e7e79a7e9c5528f9956109186
Bruno Terra <brunotftrindade@gmail.com>
2020-01-16 22:46:17 -0300

    Inicio do frontend

---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,10 @@
+import React  from "react";
+import logo from '../assets/tindev.svg';
+
+export default function Login (){
+  return (
+    <div>
+      <img src={logo} alt="Tindev"/>
+    </div>
+  );
+}
